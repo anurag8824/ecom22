dafb3d7c846787841f66caa03367677ba58894d3
rahulk67 <rahulkumawat6767@gmail.com>
2025-10-07 00:44:46 +0530

    Dfh

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 // const DEPLOYED='https://api.paze3.com/'
-// const LOCALHOST='https://ecoback2.vercel.app'
-const LOCALHOST='http://localhost:5454'
+const LOCALHOST='https://ecoback2.vercel.app'
+// const LOCALHOST='http://localhost:5454'
 
 
 export const API_BASE_URL = LOCALHOST;
