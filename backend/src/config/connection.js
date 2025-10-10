b8de64849edf13b47e573854df132d32b0700d78
rahulk67 <rahulkumawat6767@gmail.com>
2025-10-10 17:41:16 +0530

    dfd

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://developmentinfayou_db_user:DLpYMWLmn2EaWhse@cluster0.ztvpgrd.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Database successfully connected!!");
   } catch (err) {
     console.log("Error in connecting database", err);
